ec96013b2aa3800cf62afe2692c6e15b0d983a2f
ViktoriaLadyhina <vika.ladyhina@gmail.com>
2025-03-10 11:40:04 +0100

    Update Task03.js

--- src/main/java/org/telran/lecture_06_quick/tasks/Task03.js
+++ src/main/java/org/telran/lecture_06_quick/tasks/Task03.js
@@ -13,3 +13,13 @@
 // 5
 
 let results = [1, 3, 4, 3, 5, 6, 7, 7, 6, 1]
+function task_3(arr) {
+    let prize = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] <= 3){
+            prize += 1
+        }    
+    }
+    return prize;
+}
+console.log("3. Призеры олимпиады: " + task_3(results));
